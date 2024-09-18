b516e5b54500295da042006dd9d566e3a02f3588
Aurora194 <as.vargasa@uea.edu.ec>
2024-09-18 15:48:50 -0500

    Tarea de la semana 14

--- Tarea Semana 14/Cálculo de Descuento en Compras.py
+++ Tarea Semana 14/Cálculo de Descuento en Compras.py
@@ -1,6 +1,6 @@
-#Funcion para calcular de descuento en compras
+#Funcion para calcular el descuento en compras
 def calcular_descuento(monto_total, porcentaje_descuento=10):
-    "Calcula el descuento y devuelve el monto del descuento"
+    #Calcula el descuento y devuelve el monto del descuento
     descuento = monto_total * (porcentaje_descuento / 100)
     return descuento
 
@@ -10,7 +10,7 @@ descuen1 = calcular_descuento(monto_tot1)
 monto_fin1 = monto_tot1 - descuen1
 print(f"Monto total: ${monto_tot1}, Descuento: ${descuen1}, Monto final a pagar: ${monto_fin1}")
 
-# El monto total de la compra como el porcentaje de descuento.
+# El monto total de la compra como el porcentaje de descuento
 monto_tot2 = 250
 porcentaje2 = 15
 descuen2 = calcular_descuento(monto_tot2, porcentaje2)
