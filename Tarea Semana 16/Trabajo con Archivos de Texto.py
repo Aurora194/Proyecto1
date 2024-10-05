a07a244595e368ddbfa9c40703668eb5d01ce8eb
Aurora194 <as.vargasa@uea.edu.ec>
2024-10-05 14:55:57 -0500

    Tarea de la Semana 16

--- Tarea Semana 16/Trabajo con Archivos de Texto.py
+++ Tarea Semana 16/Trabajo con Archivos de Texto.py
@@ -1,25 +1,25 @@
 
 # Escritura de Archivo de Texto
 # Se crea un archivo llamado my_notes.txt
-file = open("my_notes.txt", "w")
+archi = open("my_notes.txt", "w")
 
 # Escribimos tres líneas de notas personales usando write()
-file.write("Nota 1: Recoger la ropa del patio.\n")
-file.write("Nota 2: Comprar ingredientes para el almuerzo.\n")
-file.write("Nota 3: Llamar a mamá.\n")
+archi.write("Nota 1: Recoger la ropa del patio.\n")
+archi.write("Nota 2: Comprar ingredientes para el almuerzo.\n")
+archi.write("Nota 3: Llamar a mamá.\n")
 
 # Cerramos el archivo después de escribir
-file.close()
+archi.close()
 
 # Lectura de Archivo de Texto
 # Abrimos el archivo my_notes.txt
-file = open("my_notes.txt", "r")
+archi = open("my_notes.txt", "r")
 
 # Leemos y mostramos cada línea utilizando readline()
-line = file.readline()  # Leer la primera línea
-while line:
-    print(line.strip())  # Mostramos la línea sin saltos de línea extra
-    line = file.readline()  # Leer la siguiente línea
+Archivo = archi.readline()  # Leer la primera línea
+while Archivo:
+    print(Archivo.strip())  # Mostramos la línea sin saltos de línea extra
+    Archivo = archi.readline()  # Leer la siguiente línea
 
 # Cerramos el archivo después de leer
-file.close()
+archi.close()
